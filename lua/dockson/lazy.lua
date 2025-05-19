1a29b3794e10eb698e033b2ef538aa4700119b18
Dockson <25272926+Parazoid@users.noreply.github.com>
2025-05-19 11:02:56 +0300

    add hardtime \n remove leap \n add formatting hotkey

--- lua/dockson/lazy.lua
+++ lua/dockson/lazy.lua
@@ -1,48 +1,55 @@
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable", -- latest stable release
-    lazypath,
-  })
+    vim.fn.system({
+        "git",
+        "clone",
+        "--filter=blob:none",
+        "https://github.com/folke/lazy.nvim.git",
+        "--branch=stable", -- latest stable release
+        lazypath,
+    })
 end
 
 
 vim.opt.rtp:prepend(lazypath)
 local plugins = {
     {
+        'VonHeikemen/lsp-zero.nvim',
+        branch = 'v2.x',
+        dependencies = {
+            -- LSP Support
+            { 'neovim/nvim-lspconfig' },             -- Required
+            { 'williamboman/mason.nvim' },           -- Optional
+            { 'williamboman/mason-lspconfig.nvim' }, -- Optional
+
+            -- Autocompletion
+            { 'hrsh7th/nvim-cmp' },     -- Required
+            { 'hrsh7th/cmp-nvim-lsp' }, -- Required
+            { 'L3MON4D3/LuaSnip' },     -- Required
+        }
+    },
+    {
         'nvim-telescope/telescope.nvim',
         dependencies = { 'nvim-lua/plenary.nvim' }
     },
 
-    {"nvim-treesitter/nvim-treesitter", build = ":TSUpdate"},
-    {"nvim-treesitter/playground"},
-    {"ThePrimeagen/harpoon",
+    { "nvim-treesitter/nvim-treesitter", build = ":TSUpdate" },
+    { "nvim-treesitter/playground" },
+    {
+        "ThePrimeagen/harpoon",
         branch = "harpoon2",
         dependencies = { 'nvim-lua/plenary.nvim' },
     },
     {
-      'VonHeikemen/lsp-zero.nvim',
-      branch = 'v2.x',
-      dependencies = {
-        -- LSP Support
-        {'neovim/nvim-lspconfig'},             -- Required
-        {'williamboman/mason.nvim'},           -- Optional
-        {'williamboman/mason-lspconfig.nvim'}, -- Optional
+        "m4xshen/hardtime.nvim",
+        lazy = false,
+        dependencies = { "MunifTanjim/nui.nvim" },
+        opts = {},
+    }, { 'kevinhwang91/nvim-ufo', requires = 'kevinhwang91/promise-async' },
+    { 'rebelot/kanagawa.nvim' },
 
-        -- Autocompletion
-        {'hrsh7th/nvim-cmp'},     -- Required
-        {'hrsh7th/cmp-nvim-lsp'}, -- Required
-        {'L3MON4D3/LuaSnip'},     -- Required
-      }
-    },
-    {'kevinhwang91/nvim-ufo', requires = 'kevinhwang91/promise-async'},
-    {'rebelot/kanagawa.nvim'},
-    {'ggandor/leap.nvim'},
+    --    { 'ggandor/leap.nvim' }, -- disabling until i get used to default vim motions instead
 }
 
 require("lazy").setup(plugins, opts)
-require("leap").add_default_mappings(true) -- true is for overiding plugin mappings, <S-s> to leap backwards
+--require("leap").add_default_mappings(true) -- true is for overiding plugin mappings, <S-s> to leap backwards
--- lua/dockson/remap.lua
+++ lua/dockson/remap.lua
@@ -2,3 +2,5 @@ vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 vim.keymap.set("i", "{<CR>", "{}<Esc>ha<CR><Esc><S-o>")
 vim.keymap.set("i", "<Alt><Bs>", "<Esc>l")
+vim.keymap.set("n", "<leader>pd", function() vim.diagnostic.open_float() end)
+vim.keymap.set("n", "<leader>ff", function() vim.lsp.buf.format() end)
